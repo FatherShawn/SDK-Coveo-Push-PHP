9dd983732504e567e7800dbe7664e4987af9ce71
Wim Nijmeijer <wnijmeijer@coveo.com>
2021-09-14 14:01:15 +0200

    Fixed Hipaa url

--- coveopush/CoveoConstants.php
+++ coveopush/CoveoConstants.php
@@ -104,7 +104,7 @@ class PlatformPaths{
 // ---------------------------------------------------------------------------------
 class PushApiEndpoint{
     const PROD_PUSH_API_URL = "https://api.cloud.coveo.com/push/v1";
-    const HIPAA_PUSH_API_URL = "https://api-hipaa.cloud.coveo.com/push/v1";
+    const HIPAA_PUSH_API_URL = "https://apihipaa.cloud.coveo.com/push/v1";
     const QA_PUSH_API_URL = "https://api-qa.cloud.coveo.com/push/v1";
     const DEV_PUSH_API_URL = "https://api-dev.cloud.coveo.com/push/v1";
 }
